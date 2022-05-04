33e44e5eb202c116b1dd63e8f90ab51b04158b18
IAmJustAmateur <53338060+IAmJustAmateur@users.noreply.github.com>
2022-05-04 14:49:46 +0300

    some minot changes

--- bot_json_states/breezy_bot/keyboards/create_keyboard.py
+++ bot_json_states/breezy_bot/keyboards/create_keyboard.py
@@ -1,9 +1,12 @@
-from states.load_states import Bot_State
+from breezy_bot.bot_states.bot_states import Bot_State
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
 
 def create_keyboard(state: Bot_State) -> ReplyKeyboardMarkup:
+    '''
+    create keyboard for Bot State
+    '''
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     keyboard.append(state.params['user_answer_options'])
     return keyboard
